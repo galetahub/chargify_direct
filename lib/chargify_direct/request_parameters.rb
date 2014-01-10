e484976a0f9878460387189444747d1830134da7
Igor Galeta <galeta.igor@gmail.com>
2014-01-10 12:09:29 +0200

    Encode raw_data params

--- lib/chargify_direct/request_parameters.rb
+++ lib/chargify_direct/request_parameters.rb
@@ -7,20 +7,18 @@ module ChargifyDirect
     include URIEncode
     include Signature
 
-    attr_reader :nonce, :data, :raw_data, :api_id, :api_secret
+    attr_reader :nonce, :raw_data, :api_id, :api_secret
 
     def initialize(api_id, api_secret, raw_data = {})
-      @api_id, @api_secret, raw_data = api_id, api_secret, raw_data
+      @api_id, @api_secret, @raw_data = api_id, api_secret, raw_data
     end
 
     def timestamp
       @timestamp = Time.now.to_i
     end
 
-    def raw_data=(hash)
-      raise ArgumentError, "Expected Hash, received #{hash.class}" unless hash.is_a? Hash
-      @raw_data = hash
-      @data = uri_encode_data(raw_data)
+    def data
+      @data ||= uri_encode_data(raw_data)
     end
 
     def nonce
